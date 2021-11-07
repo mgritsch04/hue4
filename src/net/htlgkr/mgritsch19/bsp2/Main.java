5c3cb4f4ecf477c09daca8799936e5441d2bda49
mgritsch04 <Gritsch.Maximilian@sus.htl-grieskirchen.at>
2021-11-07 16:05:00 +0100

    2. bsp

---
+++ src/net/htlgkr/mgritsch19/bsp2/Main.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.htlgkr.mgritsch19.bsp2;
+
+/**
+ *
+ * @author maxim
+ */
+public class Main {
+
+}
